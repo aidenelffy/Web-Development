f194254e54b6ba44075d92329e020eeafe500c9d
aidenelffy <66686974+aidenelffy@users.noreply.github.com>
2021-02-20 09:51:03 +0600

    Create main.js

---
+++ lab3/main.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('#input');
+const btn = document.querySelector('#btn');
+const result = document.querySelector('#result');
+
+btn.addEventListener('click', function() {
+    if (input.value === ' ') return
+    createDeleteElements(input.value)
+    input.value = ''
+})
+
+function createDeleteElements(value) {
+    
+    const li = document.createElement('li')
+    const btn = document.createElement('button')
+
+    li.className = 'li'
+    li.textContent = value
+
+    btn.className = 'btn'
+    btn.textContent = ''
+    li.appendChild(btn)
+
+    btn.addEventListener('click', function() {
+        result.removeChild(li)
+    })
+
+    li.addEventListener('click', (e) => {
+        li.classList.toggle('li-active')
+    })
+    result.appendChild(li)
+}
